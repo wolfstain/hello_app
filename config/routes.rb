2fdf5534e069f1eeab12598598b1295c3255101d
wolfstain <dahoyosr@unal.edu.co>
2017-09-09 21:18:51 +0000

    Static pages modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-
+  get  'static_pages/about'
   get 'static_pages/help'
 
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  #root 'application#hello'
+  root 'static_pages#home'
 end
