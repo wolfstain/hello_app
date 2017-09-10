3f73a507dcd8239e43c0a98797587ab702b55232
wolfstain <dahoyosr@unal.edu.co>
2017-09-10 23:43:37 +0000

    Cambiosrealizados

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
   def hello
     render html: "¡Hola, mundo!"
   end
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  has_many :microposts
-  validates :name, presence: true 
-  validates :email, presence: true  
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
