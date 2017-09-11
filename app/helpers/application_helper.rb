0fda0008f757884672f0c2cc0047882d453ffa07
wolfstain <dahoyosr@unal.edu.co>
2017-09-11 00:29:00 +0000

    helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
  # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "No Me Roben Mi Bici"
     if page_title.empty?
       base_title
     else
