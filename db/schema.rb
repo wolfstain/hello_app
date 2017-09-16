378fac770339b934719fa91a0c2506a9d722b28a
dahoyosr <dahoyosr@unal.edu.co>
2017-09-16 15:25:18 +0000

    eliminada configuracion defectuosa migracion

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170910170916) do
 
-  create_table "microposts", force: :cascade do |t|
-    t.text "content"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.string "email", default: "", null: false
